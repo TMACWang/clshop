f47bd89f8fbc839ff4ef9a91f4c02e87e33551cd
LuYuanGit <18392709319@163.com>
2017-11-20 14:29:04 +0800

    项目初始化

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Vue2
 
 
 ## mongod 数据库操作写篇博客
+
+### config index.js
+```
+module.exports = {
+  dev: {
+    ...
+    proxyTable: {
+
+    }
+  }
+}
+...
+```
---
+++ client/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <el-switch active-color="red" inactive-color="green" v-model="value1">
+    </el-switch>
+    <div class="block">
+      <span class="demonstration">区分颜色</span>
+      <el-rate
+        v-model="value2"
+        :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+      </el-rate>
+    </div>
+    <div :class="evenOrOdd" @click="add">
+      {{ count }}
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      value1: true,
+      value2: null
+    }
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    },
+    evenOrOdd (state) {
+      return this.$store.getters.evenOrOdd
+    }
+  },
+  methods: {
+    add () {
+      this.$store.dispatch('increment')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.even {
+  color: green;
+}
+.odd {
+  color: red;
+}
+</style>
